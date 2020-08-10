f0ae607be41c89a5c1327c1a84517407bb6e1bcd
Bert Keller <bertwinkeller@gmail.com>
2020-08-09 23:03:07 -0700

    uninstalled packages

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/index.js'
 
 function App() {
   return (
     <div className="App">
      <h1>Hello World!</h1>
+    <Home/>
     </div>
   );
 }
