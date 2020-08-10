c814aaddb8c5e52108169eb44750bc8d68955eb2
Bert Keller <bertwinkeller@gmail.com>
2020-08-09 23:54:10 -0700

    navbar working

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home/index.js'
+import NavigationBar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello World!</h1>
-    <Home/>
+     <NavigationBar/>
     </div>
   );
 }
--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
 
-const Navbar = () => {
+const Navigationbar = () => {
     return (
         <div>
-            
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Bert Keller</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#features">About</Nav.Link>
+                        <Nav.Link href="#pricing">Skills</Nav.Link>
+                        <Nav.Link href="#deets">Resume</Nav.Link>
+                        <Nav.Link href="#memes">Contact</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </div>
     )
 }
 
-export default Navbar
+export default Navigationbar
